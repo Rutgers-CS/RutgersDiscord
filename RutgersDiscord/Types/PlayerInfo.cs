97f4c84804c86520c1eacb604592c3bfeff8810b
gui <guihori2000@hotmail.com>
2022-03-10 01:53:36 -0500

    start on veto

---
+++ RutgersDiscord/Types/PlayerInfo.cs
@@ -0,0 +1,6 @@
+﻿public class PlayerInfo
+{
+    public long DiscordID { get; set; }
+    public long? SteamID { get; set; }
+    public long? TeamID { get; set; }
+}
---
+++ RutgersDiscord/Types/TeamInfo.cs
@@ -0,0 +1,8 @@
+﻿public class TeamInfo
+{
+	public string TeamName { get; set; }
+	public long TeamID { get; set; }
+	//User 1 is captain
+	public long Player1 { get; set; }
+	public long Player2 { get; set; }
+}
