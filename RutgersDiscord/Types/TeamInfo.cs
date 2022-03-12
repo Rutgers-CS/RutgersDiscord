1f7a9c767b2ce4bb4e7be470a11248e0420e27b4
guihori <guihori2000@hotmail.com>
2022-03-12 02:18:30 -0500

    finish veto

--- RutgersDiscord/Types/TeamInfo.cs
+++ RutgersDiscord/Types/TeamInfo.cs
@@ -1,6 +1,10 @@
-﻿public class TeamInfo
+﻿using Dapper.Contrib.Extensions;
+
+[Table ("team_list")]
+public class TeamInfo
 {
 	public string TeamName { get; set; }
+	[Key]
 	public long TeamID { get; set; }
 	//User 1 is captain
 	public long Player1 { get; set; }
