49622d6ae704ec9fe338d33d9b7d03a28d5ec4bd
AndrewGalifi <andrewgalifi10@gmail.com>
2022-03-22 22:02:58 -0400

    user commands
    lotta stuff

--- RutgersDiscord/Commands/User/HelpCommand.cs
+++ RutgersDiscord/Commands/User/HelpCommand.cs
@@ -37,8 +37,9 @@ namespace RutgersDiscord.Commands.User
             emotes.Add(backwardemote, PaginatorAction.Backward); emotes.Add(forwardemote, PaginatorAction.Forward);
 
             string gencommands = "/admin - Calls an admin" + "\r\n\r\n";
-            string eventcommands = "/register - Register for an event" + "\r\n\r\n" + "/ready | /unready - Readies/unreadies for a match" + "\r\n\r\n" + "/leaderboard - Displays the tourney's standings" + "\r\n\r\n" + "/stats - Displays the tourney's player stats" + "\r\n\r\n";
-        
+            string eventcommands = "/ready | /unready - Readies/unreadies for a match" + "\r\n\r\n" + "/leaderboard - Displays the tourney's standings" + "\r\n\r\n" + "/stats - Displays the tourney's player stats" + "\r\n\r\n";
+         // /reschedule  and // /veto ------ only works in match channel, only by team captain
+
             EmbedBuilder embed = new EmbedBuilder().WithTitle("Rutgers CS:GO Commands").WithColor(new Color(102, 0, 0)).AddField("General Commands", $"```{gencommands}```", true).AddField("Event Commands", $"```{eventcommands}```", false).WithThumbnailUrl("https://i.imgur.com/hePqR1R.png");
 
             await _context.Interaction.RespondAsync(embed: embed.Build(), ephemeral: true);
--- RutgersDiscord/Commands/User/StatsCommand.cs
+++ RutgersDiscord/Commands/User/StatsCommand.cs
@@ -20,11 +20,6 @@ namespace RutgersDiscord.Commands.User
         private readonly DatabaseHandler _database;
         private readonly InteractivityService _interactivity;
 
-        private List<string> killslist = new List<string> { "Kenji - 67", "August - 53", "Gal - 45", "Pat - 41", "Matt - 23", "James - 21", "Pike - 18", "Lucas - 8", "Buttermilk - 5", "Jdawg - 2", "Poopyhead - 1" };
-        private List<string> deathslist = new List<string> { "Natedogg - 59", "Jdawg - 50", "Gal - 49", "Jesse - 43", "Mustard - 26", "James - 21", "Pike - 18", "Lucas - 8", "Buttermilk - 5", "Poopyhead - 1", "Chalk - 0" };
-        private List<string> kdslist = new List<string> { "Kenji - 1.45", "August - 1.42", "Gal - 1.21", "Pat - 1.19", "Matt - 1.01", "James - 1.00", "Pike - 0.93", "Lucas - 0.85", "Buttermilk - 0.67", "Jdawg - 0.43", "Poopyhead - 0.12" };
-
-
         public StatsCommand(DiscordSocketClient client, SocketInteractionContext context, DatabaseHandler database, InteractivityService interactivity)
         {
             _client = client;
@@ -38,6 +33,70 @@ namespace RutgersDiscord.Commands.User
             string kills;
             string deaths;
             string kds;
+            List<string> killslist = new();
+            List<string> deathslist = new();
+            List<string> kdslist = new();
+            float pk;
+            float pd;
+            float kdpd;
+            float calckd;
+            long playerdiscord = 0;
+
+            List<string> MySort(List<string> list, bool isInt)
+            {
+                if (isInt)
+                {
+                    var ordered = list.Select(s => new { Str = s, Split = s.Split('-') })
+                    .OrderByDescending(x => int.Parse(x.Split[1]))
+                    .ThenBy(x => x.Split[0])
+                    .Select(x => x.Str)
+                    .ToList();
+                    return ordered;
+                }
+                else
+                {
+                    var ordered = list.Select(s => new { Str = s, Split = s.Split('-') })
+                  .OrderByDescending(x => float.Parse(x.Split[1]))
+                  .ThenBy(x => x.Split[0])
+                  .Select(x => x.Str)
+                  .ToList();
+                    return ordered;
+                }
+            }
+
+            IEnumerable<PlayerInfo> dataplayers = await _database.GetAllPlayersAsync();
+            foreach (var player in dataplayers)
+            {
+
+                if (player.Kills == null)
+                { 
+                    pk = 0;
+                } else
+                {
+                    pk = (float)player.Kills;
+                }
+                if (player.Deaths == null)
+                {
+                    pd = 0;
+                    kdpd = 1;
+                } else
+                {
+                    pd = (float)player.Deaths;
+                    kdpd = (float)player.Deaths;
+                }
+                
+                playerdiscord = player.DiscordID;
+                calckd = pk / kdpd;
+
+                killslist.Add($"<@{playerdiscord}>" + " - " + pk);
+                deathslist.Add($"<@{playerdiscord}>" + " - " + pd);
+                kdslist.Add($"<@{playerdiscord}>" + " - " + calckd.ToString("0.0#"));
+            }
+
+            killslist = MySort(killslist, true);
+            deathslist = MySort(deathslist, true);
+            kdslist = MySort(kdslist, false);
+
             List<PageBuilder> pages = new();
             Dictionary<IEmote, PaginatorAction> emotes = new Dictionary<IEmote, PaginatorAction>();
 
@@ -45,15 +104,42 @@ namespace RutgersDiscord.Commands.User
             emotes.Add(backwardemote, PaginatorAction.Backward); emotes.Add(forwardemote, PaginatorAction.Forward);
 
             var originalsize = killslist.Count; //all lists will be of equal size
-            for (int i = 0; i <= (originalsize / 10); i++)
+            if (originalsize % 10 == 0)
             {
-                kills = string.Join("\r\n", killslist.Take(10));
-                deaths = string.Join("\r\n", deathslist.Take(10));
-                kds = string.Join("\r\n", kdslist.Take(10));
-                pages.Add(new PageBuilder().WithTitle("Scarlet Classic's Statistical Leaders").WithColor(new Color(102, 0, 0)).WithFooter("Rutgers CS:GO").AddField("Most Kills", $"```{kills}```", true).AddField("Most Deaths", $"```{deaths}```", true).AddField("Highest KD", $"```{kds}```", false).WithThumbnailUrl("https://imgur.com/Fb2i1rI.png"));
-                killslist.RemoveRange(0, Math.Min(10, killslist.Count));
-                deathslist.RemoveRange(0, Math.Min(10, deathslist.Count));
-                kdslist.RemoveRange(0, Math.Min(10, kdslist.Count));
+                for (int i = 0; i < (originalsize / 10); i++)
+                {
+                    kills = string.Join("\r\n", killslist.Take(10));
+                    deaths = string.Join("\r\n", deathslist.Take(10));
+                    kds = string.Join("\r\n", kdslist.Take(10));
+                    pages.Add(new PageBuilder()
+                        .WithTitle("Scarlet Classic's Statistical Leaders")
+                        .WithColor(new Color(102, 0, 0))
+                        .WithFooter("Rutgers CS:GO")
+                        .AddField("Most Kills", $"{kills}", true)
+                        .AddField("Most Deaths", $"{deaths}", true)
+                        .AddField("Highest KD", $"{kds}", true));
+                    killslist.RemoveRange(0, Math.Min(10, killslist.Count));
+                    deathslist.RemoveRange(0, Math.Min(10, deathslist.Count));
+                    kdslist.RemoveRange(0, Math.Min(10, kdslist.Count));
+                }
+            } else
+            {
+                for (int i = 0; i <= (originalsize / 10); i++)
+                {
+                    kills = string.Join("\r\n", killslist.Take(10));
+                    deaths = string.Join("\r\n", deathslist.Take(10));
+                    kds = string.Join("\r\n", kdslist.Take(10));
+                    pages.Add(new PageBuilder()
+                        .WithTitle("Scarlet Classic's Statistical Leaders")
+                        .WithColor(new Color(102, 0, 0))
+                        .WithFooter("Rutgers CS:GO")
+                        .AddField("Most Kills", $"{kills}", true)
+                        .AddField("Most Deaths", $"{deaths}", true)
+                        .AddField("Highest KD", $"{kds}", true));
+                    killslist.RemoveRange(0, Math.Min(10, killslist.Count));
+                    deathslist.RemoveRange(0, Math.Min(10, deathslist.Count));
+                    kdslist.RemoveRange(0, Math.Min(10, kdslist.Count));
+                }
             }
             var paginator = new StaticPaginatorBuilder().WithUsers(_context.User).WithPages(pages).WithFooter(PaginatorFooter.PageNumber | PaginatorFooter.Users).WithEmotes(emotes).Build();
 
