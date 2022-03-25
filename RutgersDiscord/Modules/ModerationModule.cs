228fd5751e71631f11a3b22299d869c42446d13d
August Seiple <aseiple678@gmail.com>
2022-03-25 01:26:58 -0400

    finish game finished

--- RutgersDiscord/Modules/ModerationModule.cs
+++ RutgersDiscord/Modules/ModerationModule.cs
@@ -290,58 +290,5 @@ namespace RutgersDiscord.Modules
         {
             //TODO
         }
-
-
-        private static readonly HttpClient client = new HttpClient();
-        [SlashCommand("temp", "temp", runMode: RunMode.Async)]
-        public async Task Temp()
-        {
-            await Context.Interaction.RespondAsync("a", ephemeral: true);
-            client.DefaultRequestHeaders.Accept.Clear();
-            //client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json");
-            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-            //var stringTask = client.GetStringAsync("https://eo6dbp93xq42the.m.pipedream.net);
-
-
-            PlayerInfo p = new PlayerInfo(1, 2, "a","b");
-
-            string json = JsonConvert.SerializeObject(p);
-
-            StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-
-            var reply = await client.PostAsync("https://eo6dbp93xq42the.m.pipedream.net", httpContent);
-
-            Console.WriteLine("asdasdasdas");
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://127.0.0.1:25565/api/");
-            listener.Prefixes.Add("http://127.0.0.1:25565/");
-
-            listener.Start();
-            HttpListenerContext context = listener.GetContext();
-            HttpListenerRequest request = context.Request;
-
-            HttpListenerResponse response = context.Response;
-
-            StreamReader sr = new StreamReader(request.InputStream, request.ContentEncoding);
-            string line;
-            // Read and display lines from the file until the end of
-            // the file is reached.
-            line = sr.ReadToEnd();
-            Console.WriteLine(line);
-
-
-            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            // Get a response stream and write the response to it.
-            response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer, 0, buffer.Length);
-            // You must close the output stream.
-            output.Close();
-            listener.Stop();
-        }
-        
     }
 }
