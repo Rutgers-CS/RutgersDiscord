99d92f0bbc4856540e80d77cfa2fd25a6cfeab8f
August Seiple <aseiple678@gmail.com>
2022-03-25 02:52:36 -0400

    p2

---
+++ RutgersDiscord/Types/ServerTokens.cs
@@ -0,0 +1,17 @@
+﻿using Dapper.Contrib.Extensions;
+[Table("servertokens")]
+public class ServerTokens
+{
+    public int id { get; set; }
+    public string token { get; set; }
+    public int inuse { get; set; }
+
+    private ServerTokens() {}
+
+    public ServerTokens(int id, string token, int inuse)
+    {
+        this.id = id;
+        this.token = token;
+        this.inuse = inuse;
+    }
+}
