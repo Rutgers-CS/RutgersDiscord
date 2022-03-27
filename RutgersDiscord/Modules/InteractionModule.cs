3925ebc91202a6a3e66c04cc732cd50d1352dd42
August Seiple <aseiple678@gmail.com>
2022-03-27 19:34:29 -0400

    Leaderboard hotfix

--- RutgersDiscord/Modules/InteractionModule.cs
+++ RutgersDiscord/Modules/InteractionModule.cs
@@ -88,8 +88,16 @@ namespace RutgersDiscord.Modules
         [SlashCommand("leaderboard", "Display the tournament leaderboard")]
         public async Task DisplayLeaderboard()
         {
-            LeaderboardCommand lc = new LeaderboardCommand(_client, Context, _database, _interactivity);
-            await lc.PullLeaderboard();
+            try
+            {
+                LeaderboardCommand lc = new LeaderboardCommand(_client, Context, _database, _interactivity);
+                await lc.PullLeaderboard();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
         }
 
         [SlashCommand("help", "Display all user commands")]
